e40aacf41973916090b27beb6a48de73c9c8827b
Phil Capon <phil.capon@fastmail.co.uk>
2022-01-25 22:50:04 +0000

    run game in Main

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using ArmchairCricket.Gameplay;
+
 Console.WriteLine("Hello, World!");
+
+
+var game = new ArmchairCricket.Gameplay.Game(); 
+var systemStrategy = new Strategy(game);
+game.PlayerOneBatting = false;
+
+while(true) {
+    game.ChoosePlayerOneCard(0);
+
+    int systemCardIndex = systemStrategy.ChoosePlayerTwoCard();
+
+    game.ChoosePlayerTwoCard(systemCardIndex);
+
+    Console.WriteLine(game.GetBallOutcome().ToString());
+
+    Console.WriteLine(game.GetLatestScore());
+    if (game.IsEndOfInnings())
+    {
+        Console.WriteLine("Game over");
+        break;
+    }
+
+}
