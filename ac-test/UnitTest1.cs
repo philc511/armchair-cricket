2ad9bc81d67f7f5b20812182c15b596716543a82
Phil Capon <phil.capon@fastmail.co.uk>
2022-02-15 23:12:50 +0000

    first unit test

--- ac-test/UnitTest1.cs
+++ ac-test/UnitTest1.cs
@@ -1,5 +1,5 @@
 using Xunit;
-
+using ArmchairCricket.Gameplay;
 namespace ac_test;
 
 public class UnitTest1
@@ -7,6 +7,7 @@ public class UnitTest1
     [Fact]
     public void Test1()
     {
-
+        BattingStrategy strategy = new BattingStrategy(null);
+        strategy.ChooseCard(new Card(Suit.Balls, 10));
     }
 }
